91927e6e2293888ed814d0cee3dde67a6d48159d
TeamSocialDanger@gmail.com <TeamSocialDanger@gmail.com>
2014-07-02 20:41:52 -0400

    This is A chat system
    Developing a chat system

---
+++ ChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        /// <summary>
+        /// The bellow section 
+        ///call a function sendmessagetopage
+        ///on all the clients that are connected
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="message"></param>
+        public void Send(string name, string message)
+        {
+            Clients.All.SendMessageToPage(name, message);
+
+           
+        
+        }
+    }
+}
